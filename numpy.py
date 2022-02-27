80961514458a14f78a76be6a15424111f1946eaf
maxwynne <76392789+maxwynne@users.noreply.github.com>
2022-02-27 12:45:07 +0000

    Update numpy.py

--- numpy.py
+++ numpy.py
@@ -1,5 +1,5 @@
 # create list of weights from a list to a Numpy array. Then, convert all of the weights from kilograms to pounds. Use the scalar conversion
-of 2.2 lbs per kilogram to make your conversion. Lastly, print the resulting array of weights in pounds.
+# of 2.2 lbs per kilogram to make your conversion. Lastly, print the resulting array of weights in pounds.
 
 weight_kg = [81.65, 97.52, 95.25, 92.98, 86.18, 88.45]
 import numpy as np
